879e5b8fff2582566a66a1c6cadb3d325c74d250
Jose Ramirez <joseramirez@Joses-MacBook-Pro.local>
2022-06-21 13:37:27 -0600

    Lower, Equal or higher than 5

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Lower, Equal or higher than 5");
+
+Console.WriteLine("Enter a Number");
+
+string input = Console.ReadLine() ?? "n";
+int number = Int32.Parse(input);
+if (number < 5)
+{
+     Console.WriteLine("The number is less than 5");
+} else {
+    if (number == 5)
+{
+     Console.WriteLine("The number is equal to 5");
+} else {
+    Console.WriteLine("The number is higher than 5");
+}
+}
